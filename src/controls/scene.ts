3fd439828d1e7fc5ad0c64494bf2c35d2d613e6b
amin.hasanzadeh <amin.hsnzadehh@gmail.com>
2026-07-14 10:30:27 +0330

    Added basic scene control, fixed camera positions, sky control, primary scene

---
+++ src/controls/scene.ts
@@ -0,0 +1,21 @@
+// controls/scene.ts
+import { useControls } from "leva";
+
+export default function useSceneController() {
+    const controls = useControls("Scene Controls", {
+        azimuth: {
+            value: 90,
+            min: 0,
+            max: 360,
+            step: 1
+        },
+        elevation: {
+            value: 15,
+            min: 0,
+            max: 90,
+            step: 1
+        }
+    });
+
+    return controls;
+}
---
+++ src/scene/main.tsx
@@ -0,0 +1,27 @@
+// MainScene.tsx
+import { useMemo } from "react";
+import * as THREE from "three";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Sky } from "@react-three/drei";
+import useSceneController from "../controls/scene.ts";
+
+export default function MainScene() {
+    const { azimuth, elevation } = useSceneController();
+
+    const sunPosition = useMemo(() => {
+        const phi = THREE.MathUtils.degToRad(90 - elevation);
+        const theta = THREE.MathUtils.degToRad(azimuth);
+        return new THREE.Vector3().setFromSphericalCoords(1, phi, theta);
+    }, [azimuth, elevation]);
+
+    return (
+        <Canvas style={{ width: '100%', height: '100vh' }} camera={{ position: [100, 120, 100], fov: 60 }}>
+            <ambientLight intensity={0.5} />
+            <directionalLight position={[100, 100, 50]} intensity={1} />
+
+            <Sky sunPosition={sunPosition} />
+
+            <OrbitControls maxPolarAngle={2} maxDistance={400} />
+        </Canvas>
+    );
+}
